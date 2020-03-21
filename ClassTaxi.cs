9194991c7709dec95117bddfe81f785a25059123
Agus Nugroho Setiawan <61860040+AgusNugros@users.noreply.github.com>
2020-03-21 17:59:39 +0700

    Add files via upload

---
+++ ClassTaxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace LatihanClassDanObject
+{
+	public class Taxi
+	{
+		// Properties
+		public string DriverName { get; set; }
+		public bool OnDuty = true;
+		public int NumPassenger { get; set; }
+
+		// method
+		public void TaxiInfo()
+		{
+			Console.WriteLine("Driver Name: {0}", DriverName);
+			Console.Write("On Duty: ");
+			Console.WriteLine(OnDuty ? "Yes" : "No");
+			Console.WriteLine("Num of Passenger: {0}", NumPassenger);
+			
+		}
+
+		public void PickupPassenger()
+		{
+			Console.WriteLine("{0} sedang menjemput penumpang ", DriverName);
+		}
+
+		public void DropOffPassenger()
+		{
+			Console.WriteLine("{0} selesai mengantar penumpang ", DriverName);
+		}
+
+	}
+}
---
+++ ProgramTaxi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LatihanClassDanObject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Taxi taxi = new Taxi();
+			taxi.DriverName = "Kang Taxi";
+			taxi.OnDuty = true;
+			taxi.NumPassenger = 10;
+
+			taxi.TaxiInfo();
+			taxi.PickupPassenger();
+			taxi.DropOffPassenger();
+
+			Console.ReadKey();
+		}
+
+	}
+}
