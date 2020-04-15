c005b86834d11bdcd13a6e842dbd72c0919932a2
Agus Nugroho Setiawan <61860040+AgusNugros@users.noreply.github.com>
2020-04-15 14:46:52 +0700

    Add files via upload

---
+++ ResponsiPemrograman2897.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace ResponsiPemrograman2596
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Karyawan karyawan = new Karyawan(19112897, "Agus", 30000000);
+            Karyawan karyawan2 = new Karyawan(19112897, "Setiawan", 2000000);
+
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan.showKaryawan();
+            karyawan2.showKaryawan();
+
+            Console.WriteLine("\n\n Data Kenaikan Gaji anda");
+
+            Console.WriteLine("NIK \t Nama \t Gaji");
+            Console.WriteLine("--------------------------");
+            karyawan.naikGaji();
+            karyawan2.naikGaji();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Karyawan
+    {
+        public int nik { get; set; }
+        public string nama { get; set; }
+        public int gajiBulanan { get; set; }
+
+        public Karyawan(int nik, string nama, int gaji)
+        {
+            this.nik = nik;
+            this.nama = nama;
+
+            if(gaji < 0)
+            {
+                this.gajiBulanan = 0;
+            } else
+            {
+                this.gajiBulanan = gaji;
+            }
+        }
+
+        public void showKaryawan()
+        {
+            Console.WriteLine("{0} \t {1} \t {2}", nik, nama, gajiBulanan);
+        }
+
+        public void naikGaji()
+        {
+            double tmp = 0;
+            tmp =  0.1 * gajiBulanan;
+            gajiBulanan += Convert.ToInt32(tmp);
+            Console.WriteLine("{0} \t {1} \t {2}", nik, nama, gajiBulanan);
+        }
+    }
+}
